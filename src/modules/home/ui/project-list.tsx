61488b415118483d05626dd937f2b70b59f40762
Vasile Draguța <dragutavasile@gmail.com>
2025-10-25 11:17:45 +0300

    oops

--- src/modules/home/ui/project-list.tsx
+++ src/modules/home/ui/project-list.tsx
@@ -11,8 +11,6 @@ export const ProjectList = () => {
   const trpc = useTRPC();
   const { data: projects } = useQuery(trpc.projects.getMany.queryOptions());
 
-  projects?.sort;
-
   return (
     <div className='w-full bg-white dark:bg-sidebar rounded-xl p-8 border flex flex-col gap-y-6 sm:gap-y-4'>
       <h2 className='text-2xl font-semibold'>Previous projects</h2>
